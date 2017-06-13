edb78c3609864ea0629a59999f7ccc77b6b3bbab
FARFETCH\jose.cardoso <jose.cardoso@farfetch.com>
2017-06-13 08:48:44 +0100

    Refactored lifecycle attachment Added lifecycle binding methods

--- app/src/main/java/com/example/app/core/BaseFragment.java
+++ app/src/main/java/com/example/app/core/BaseFragment.java
@@ -1,6 +1,9 @@
 package com.example.app.core;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 /**
  * Created by jose.cardoso on 06-06-2017.
@@ -11,16 +14,16 @@ public class BaseFragment<T extends BasePresenter> extends Fragment {
     protected T mPresenter;
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         if (mPresenter != null) {
             mPresenter.onViewAttached();
         }
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onDestroyView() {
+        super.onDestroyView();
         if (mPresenter != null) {
             mPresenter.onViewDetached();
         }
